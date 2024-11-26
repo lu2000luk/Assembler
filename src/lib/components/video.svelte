a1a67173236fcc1560cea23e03a7c341112d0437
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2024-11-26 17:25:21 +0100

    Update video.svelte

--- src/lib/components/video.svelte
+++ src/lib/components/video.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <!-- svelte-ignore a11y_media_has_caption -->
-<video class="component video input border-2 rounded-lg border-accent focus:border-secondary outline-none bg-background p-2" id={id} width={width} height={height}>
+<video class="component video input border-2 rounded-lg border-accent focus:border-secondary outline-none bg-background p-2" id={id} width={width} height={height} controls>
     <source src={src} type="video/mp4">
     Your browser does not support the video tag.
 </video>
